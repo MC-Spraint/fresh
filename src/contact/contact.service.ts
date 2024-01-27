dd1aae740a8ae030b75adb839805bca51764698b
MC_Spraint <snahashis@pietechsolution.com>
2024-01-27 17:38:57 +0530

    add exception in save contact

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,5 +1,5 @@
 import { ContactRepository } from "./contact.repository";
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable } from "@nestjs/common";
 import { SaveContactRequest } from "./dto/request/save-contact.request";
 import { Contact } from "./entity/contact.entity";
 
@@ -7,6 +7,12 @@ import { Contact } from "./entity/contact.entity";
 export class ContactService {
   constructor(private readonly _contactRepo: ContactRepository) {}
   async saveContact(contact: SaveContactRequest): Promise<Contact> {
+    const existingContact = await this._contactRepo.findOne({
+      email: contact.email,
+    });
+    if (existingContact) {
+      throw new HttpException("Already Exists", 409);
+    }
     const savedContact = await this._contactRepo.createAndSave(contact);
     return savedContact;
   }
